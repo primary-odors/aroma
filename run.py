32a84c0c85d893970e796ba4267b4a70c5664310
Julie Gagnon <julie@umop.net>
2026-03-12 10:14:05 -0700

    cavity bottom

--- run.py
+++ run.py
@@ -208,12 +208,12 @@ for rcpid in data.protutils.prots.keys():
 
         cavfna = f"pdbs/{fam}/{rcpid}.active.cvty"
         cavfni = cavfna.replace(".active.", ".inactive.")
-        if not os.path.exists(cavfna):
+        if 1: # not os.path.exists(cavfna):
             cmd = ["bin/cavity_search", "-p", f"pdbs/{fam}/{rcpid}.active.pdb", "-o", cavfna,
                 "--yminr", "3.40", "--ymaxr", "45.52", "--sr", "3.28", "--er", "7.48"]
             print(" ".join(cmd))
             subprocess.run(cmd)
-        if not os.path.exists(cavfni):
+        if 1: # not os.path.exists(cavfni):
             cmd = ["bin/cavity_search", "-p", f"pdbs/{fam}/{rcpid}.inactive.pdb", "-o", cavfni,
                 "--yminr", "3.40", "--ymaxr", "45.52", "--sr", "3.28", "--er", "7.48"]
             print(" ".join(cmd))
